e9b26c985896e033afbe94d08291041270c68cc9
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-08 14:16:32 -0500

    added author and post models

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog_app.models import Post, Author
+
+admin.site.register([Author, Post])
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField('auth.User')
+
+    def __str__(self):
+        return "{}'s Author instance".format(self.user.username)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    author = models.ForeignKey(Author)
+    posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.title, self.author.user.username)
+
+
+@receiver(post_save, sender='auth.User')
+def create_author(sender, **kwargs):
+    print(kwargs)
+    user_instance = kwargs.get('instance')
+    created = kwargs.get('created')
+    if created:
+        Author.objects.create(user=user_instance)
